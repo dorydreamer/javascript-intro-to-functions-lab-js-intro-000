58598ace0f45916a054db4e2d54fb7fe929f5093
Doris Le <dorisle@sandiego.edu>
2020-01-08 05:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function sayHiToGrandma(string) {
   var mixedcase = "I love you, Grandma."
   if (string.toLowerCase() === lowercase) {
     return 'I can\'t hear you'
+} else if (string.toUpperCase() === uppercase) {
+  return "YES INDEED"
+} else if (string === mixedcase) {
+  return "I love you, too."
 }
 }
