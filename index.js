da6beefd15848d0896e3478827dd60f82e7b7a2a
Doris Le <dorisle@sandiego.edu>
2020-01-08 06:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = 'hello';
+  var lowercase = 'hi';
   if (string.toLowerCase() === lowercase) {
     return "I can't hear you!";
 }
